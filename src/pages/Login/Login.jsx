a93130b05e1acdb3ccc965da6a2089b63f25f0b5
zer0p01nt <1027.bmy@gmail.com>
2025-11-03 01:44:19 +0900

    ✨ Feat: ButtonGroup 컴포넌트 구현

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,3 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import ButtonGroup from "../../components/button-group/ButtonGroup";
+
 export default function Login() {
-  return <div>로그인 페이지</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div>로그인 페이지</div>
+      <ButtonGroup
+        grayText="Back"
+        onGrayClick={() => navigate(-1)}
+        blueType="button"
+        blueText="Next"
+        blueDisabled={false}
+        onBlueClick={() => navigate("/")}
+      />
+    </>
+  );
 }
