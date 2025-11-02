91f579da0ba46e0e5ce4462cbdb1e9c2411e154d
zer0p01nt <1027.bmy@gmail.com>
2025-11-03 01:43:48 +0900

    ✏️ Typo: 주석 수정

--- src/stores/headerStore.js
+++ src/stores/headerStore.js
@@ -7,18 +7,16 @@ const DEFAULT_HEADER_CONFIG = {
   showSettingBtn: false,
 };
 
-/**
- * 사용할 땐 아래와 같이
-  import { useHeaderStore } from "../../stores/headerStore";
-  const setHeaderConfig = useHeaderStore((state) => state.setHeaderConfig);
-  useEffect(() => {
-    setHeaderConfig({
-      title: "헤더 텍스트",
-      showBackBtn: false, // 뒤로가기 버튼 여부
-      showSettingBtn: false, // 환경설정 버튼 여부
-    });
-  }, [setHeaderConfig]);
- */
+// 사용할 땐 아래와 같이
+// import { useHeaderStore } from "../../stores/headerStore";
+// const setHeaderConfig = useHeaderStore((state) => state.setHeaderConfig);
+// useEffect(() => {
+//   setHeaderConfig({
+//     title: "헤더 텍스트",
+//     showBackBtn: false, // 뒤로가기 버튼 여부
+//     showSettingBtn: false, // 환경설정 버튼 여부
+//   });
+// }, [setHeaderConfig]);
 export const useHeaderStore = create((set) => ({
   headerConfig: DEFAULT_HEADER_CONFIG,
   setHeaderConfig: (newConfig) =>
