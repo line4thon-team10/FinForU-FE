5e296d31df64e4c52db35b030db4dfecb309f094
zer0p01nt <1027.bmy@gmail.com>
2025-11-03 23:58:18 +0900

    ✨ Feat: Navigation 컴포넌트 다국어 처리 추가

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,33 +1,35 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { GuideIcon, MapIcon, ProductIcon, RatesIcon, WalletIcon } from "./NavigationIcon";
 import * as S from "./NavigationStyle";
+import { useTranslation } from "react-i18next";
 
 export default function Navigation() {
   const location = useLocation();
   const navigate = useNavigate();
+  const { t } = useTranslation();
   const isActive = (path) => location.pathname === path;
 
   return (
     <S.Container>
       <S.Button $isActive={isActive("/guide")} onClick={() => navigate("/guide")}>
         <GuideIcon />
-        <div>Guide</div>
+        <div>{t("nav.guide")}</div>
       </S.Button>
       <S.Button $isActive={isActive("/map")} onClick={() => navigate("/map")}>
         <MapIcon />
-        <div>Map</div>
+        <div>{t("nav.map")}</div>
       </S.Button>
       <S.Button $isActive={isActive("/product")} onClick={() => navigate("/product")}>
         <ProductIcon />
-        <div>Product</div>
+        <div>{t("nav.product")}</div>
       </S.Button>
       <S.Button $isActive={isActive("/rates")} onClick={() => navigate("/rates")}>
         <RatesIcon />
-        <div>Rates</div>
+        <div>{t("nav.rates")}</div>
       </S.Button>
       <S.Button $isActive={isActive("/wallet")} onClick={() => navigate("/wallet")}>
         <WalletIcon />
-        <div>Wallet</div>
+        <div>{t("nav.wallet")}</div>
       </S.Button>
     </S.Container>
   );
