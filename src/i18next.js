b937fa2812d0799edaf6d6bf95d5aa5f233a720d
zer0p01nt <1027.bmy@gmail.com>
2025-11-03 17:37:04 +0900

    🔧 Chore: i18next 초기 세팅

---
+++ src/i18next.js
@@ -0,0 +1,29 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./locales/en.json";
+import zh from "./locales/zh.json";
+import vi from "./locales/vi.json";
+
+i18next.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: en,
+    },
+    zh: {
+      translation: zh,
+    },
+    vi: {
+      translation: vi,
+    },
+  },
+  // 기본 언어 설정 : en
+  lng: "en",
+  // react-i18next 처리 로그 콘솔 출력 설정
+  debug: true,
+  // 동적인 데이터 값 할당 설정
+  interpolation: {
+    escapeValue: false, // react는 XSS에 안전하기 때문에 false로 설정
+  },
+});
+
+export default i18next;
