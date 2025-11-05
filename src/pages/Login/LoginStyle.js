67f5634d8db4c38be22389e745b6355d57d4f63b
zer0p01nt <1027.bmy@gmail.com>
2025-11-06 05:29:38 +0900

    ♻️ Refactor: css 및 코드 일부 수정

--- src/pages/Login/LoginStyle.js
+++ src/pages/Login/LoginStyle.js
@@ -57,6 +57,7 @@ export const Form = styled.form`
     font-size: 1.125rem;
     font-weight: 600;
     color: #000;
+    outline: none;
 
     &::placeholder {
       color: var(--color-input-gray);
@@ -68,14 +69,12 @@ export const EmailInput = styled.input`
   border-radius: 0.625rem 0.625rem 0 0;
   border: 2px solid var(--color-input-gray);
   border-bottom: 1px solid var(--color-input-gray);
-  outline: none;
 `;
 
 export const PWInput = styled.input`
   border-radius: 0 0 0.625rem 0.625rem;
   border: 2px solid var(--color-input-gray);
   border-top: 1px solid var(--color-input-gray);
-  outline: none;
 `;
 
 export const Btn = styled.button`
