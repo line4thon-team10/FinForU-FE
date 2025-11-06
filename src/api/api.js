94583eb9f9e1af71188ca23b521af1132762b926
zer0p01nt <1027.bmy@gmail.com>
2025-11-06 18:50:02 +0900

    🐛 Fix: 현재 언어 지정하지 않은 경우 기본값 영어로 서버와 통신하게 수정

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import i18next from "i18next";
+import { LANG_MAP } from "../constants/languageMap";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -8,7 +9,8 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    const currentLang = i18next.language;
+    // 현재 언어를 지정하지 않은 경우 자동으로 영어로
+    const currentLang = LANG_MAP[i18next.language] || "ENGLISH";
     config.headers["Accept-Language"] = currentLang;
     return config;
   },
